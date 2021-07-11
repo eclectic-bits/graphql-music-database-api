941b9558312dd44a66030fa0c9d69a22534a2720
Michael Colby <mikescolby@outlook.com>
2021-07-10 21:47:57 -0500

    adding comments to middleware.ts

--- src/types/middleware.ts
+++ src/types/middleware.ts
@@ -1,3 +1,6 @@
 import { Request, Response } from 'express';
 
+/**
+ * Middleware type for use with expressJS middleware
+ */
 export declare type Middleware = (request: Request, response: Response)=> Promise<void>;
